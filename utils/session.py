6b3056731e80ffe9ab7f789edbe37c7ecb2a9a6f
lucays <lucahhai@gmail.com>
2021-11-28 01:52:17 +0800

    feat: add proxy option

--- utils/session.py
+++ utils/session.py
@@ -1,10 +1,19 @@
+import pyppeteer
 
 from requests_html import AsyncHTMLSession
 
 
+class AsyncHTMLSession2(AsyncHTMLSession):
+
+    @property
+    async def browser(self):
+        if not hasattr(self, "_browser"):
+            return await pyppeteer.launch(headless=True, args=['--no-sandbox', '--proxy-server=<your proxy or delete this arg>'])
+
+
 class AsyncSession:
     def __init__(self):
-        self.asession = AsyncHTMLSession()
+        self.asession = AsyncHTMLSession2()
 
     async def __aenter__(self):
         return self.asession
