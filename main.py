be9954b2a02427cb9c08bd9ca4dfaaf07a7659dd
luca <lucahhai@gmail.com>
2021-12-05 23:31:20 +0800

    fix: rebuild table

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import time
 import asyncio
 
 from spider import CardDBSpider
-from rebuild import check_crete_tables
+from rebuild import async_create_tables
 
 
 if __name__ == '__main__':
     while True:
         card_spider = CardDBSpider()
-        asyncio.run(check_crete_tables())
+        asyncio.run(async_create_tables())
         asyncio.run(card_spider.get_cards())
         time.sleep(3600*6)
--- rebuild.py
+++ rebuild.py
@@ -1,27 +1,18 @@
-from sqlalchemy.sql.expression import select
 
-from models import Card
-from models.dependencies import Base, engine, async_session
+from models.dependencies import Base, engine
 
 
 async def async_create_tables():
 
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     await engine.dispose()
 
 
-async def check_crete_tables():
-    need_rebuild = True
-    async with async_session() as session:
-        async with session.begin():
-            cards = await session.execute(select(Card).limit(1))
-            card = cards.scalars().first()
-            if card:
-                need_rebuild = False
-    if need_rebuild:
-        await async_create_tables()
+async def rebuild_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
 if __name__ == '__main__':
